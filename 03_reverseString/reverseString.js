7ce7b59e4b69212d602edccae6e35f17fdb57715
freshdatabit <130305946+freshdatabit@users.noreply.github.com>
2023-05-12 18:25:29 +0800

    finishing exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    /*
+    const strArray = str.split('');
+    const reversedArray = strArray.reverse();
+    const joinedArray = reversedArray.join('');
+    return joinedArray;
+    */
+   const reversed = str.split('').reverse().join('');
+   return reversed;
 };
 
+console.log(reverseString('what the hell'))
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...args){
+    return array.filter(function(any){
+        return !args.includes(any);
+    })
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
