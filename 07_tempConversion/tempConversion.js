f74c0d0e291d8316148a4b3cab21368cb0fc683e
freshdatabit <130305946+freshdatabit@users.noreply.github.com>
2023-06-08 10:53:09 +0800

    Just playing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celsius) {
+  return (celsius - 32) * 5/9
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheit) {
+  return (fahrenheit * 9/5) +32
 };
 
 // Do not edit below this line
